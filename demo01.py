7ae7d5974aa105ebec861b5564f60f072953fa4c
王薇薇 <sfy_mx@163.com>
2021-01-29 17:46:17 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,31 @@
+#print("hello world")
+#得到的
+
+
+#print(((1+2)*100-99)/2)
+#print(2>3)
+
+# a = float(input("请输入："))
+# b = float(input("请输入："))
+# print("input获取的内容",a+b)
+
+# print(type(12222))
+
+# a = int("23333")
+# print(type(a))
+
+
+# a = 'dasfdsgfdsgafsdfsdafdsf'
+# print(len(a))
+
+#练习：通过代码获取两段内容，并且计算他们长度的和
+#今天所讲内容用word文档整理成笔记
+
+
+# a = str(input("请输入a的值："))
+# b = str(input("请输入b的值："))
+# print("a+b的长度为：",len(a+b))
+
+a = input("请输入：")
+b = input("请输入：")
+print("两段字符串的长度是：",len(a+b))
